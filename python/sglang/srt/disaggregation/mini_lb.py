ca9686f036d3dc4a8ac5d74a9f6c3a28ffd06bab
neo.zty <moyun.zty@antgroup.com>
2025-04-18 19:29:28 +0800

    add print

--- python/sglang/srt/disaggregation/mini_lb.py
+++ python/sglang/srt/disaggregation/mini_lb.py
@@ -159,6 +159,7 @@ async def handle_generate_request(request_data: dict):
     modified_request = request_data.copy()
     req_id = modified_request.get("req_id", None)
     bootstrap_room = req_id if req_id is not None else random.randint(0, 2**63 - 1)
+    print(f"bootstrap_room: {bootstrap_room}")
     modified_request.update(
         {
             "bootstrap_host": hostname,
